6ae5ae296386d7b8e135f067a0a40a6360ddf6be
Vennela, Tatavarthy S <vennela_tatavarthy@optum.com>
2024-05-02 14:39:28 +0530

    k

---
+++ App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+const heading= <h1>Krishna Loves Radha</h1>
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
